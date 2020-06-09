f29b6d2183e6d87f0570f41f173fe8711404d3c0
prahlad6290 <66668258+prahlad6290@users.noreply.github.com>
2020-06-09 16:34:37 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+function fee()
+{
+    var x=document.getElementById(se).insertAdjacentText;
+    var y;
+    if(x==se)
+    {
+    y=URL("https://www.google.com");
+    document.getElementById(s).value=y;
+    }
+
+}
